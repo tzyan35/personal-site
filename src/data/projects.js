028d89e6abce0d200e2ef6f1cfeea3c23ef8bfdb
tzyan35 <44307956+tzyan35@users.noreply.github.com>
2022-06-02 01:20:58 +0800

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,6 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
-    desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
-  },
-  {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
-    desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
-  },
-  {
     title: 'Food Recipe Search',
     subtitle: 'A website to search for different food recipes.',
     link: 'https://foodrecipe99.herokuapp.com/',
