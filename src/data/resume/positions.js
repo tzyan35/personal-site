3ad3e4d0e52291f10cd34c30cdc27b6935440f2a
tzyan35 <zhongyan1997@hotmail.com>
2022-06-06 04:13:27 +0800

    commit

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -4,9 +4,9 @@ const positions = [
     position: 'Student Assistant at Nanyang House',
     daterange: 'August 2019 - April 2021',
     points: [
-      'Liased with different student groups in NTU to provide sufficient equipment and resource for their event.',
-      'Accounted and oversaw transportation of big-ticket items around campus such as grand piano, speakers and audio equipment.',
-      'Conducted occasional inspection on building facilities and rectified imminent problems immediately.',
+      'Liaised with different student groups in NTU to provide sufficient equipment and resource for their event.',
+      'Accounted for and oversaw the transportation of big-ticket items around campus such as grand piano, speakers and audio equipment.',
+      'Conducted occasional inspections of building facilities and rectified imminent problems immediately.',
     ],
   },
   {
@@ -16,7 +16,7 @@ const positions = [
     daterange: 'June 2020 - Present',
     points: [
       'Composed and write piano sheet music for clients all around the world.',
-      'Comissioned custom music related gigs for aspiring musicians such as composing a piano sheet by ear or converting electronic dance music into piano sheets.',
+      'Commissioned custom music-related gigs for aspiring musicians such as composing a piano sheet by ear or converting electronic dance music into piano sheets.',
       'Converted piano sheet music into mp4 and synthesia style format using state of the art music software.',
     ],
   },
@@ -26,7 +26,7 @@ const positions = [
     daterange: 'May 2016 - Nov 2017',
     points: [
       'Provided 24 Hour Survellience to Key Areas in Singapore.',
-      'Escorted vital army resources from one location to another swiftly without hindrance.',
+      'Escorted vital army resources from one location to another swiftly and professionally.',
     ],
   },
 ];
